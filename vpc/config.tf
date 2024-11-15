35c393cd8697beac3feaf9b54f5973fe141bcd85
Swaroop <swaroopiit@gmail.com>
2024-11-15 10:49:26 +0530

    Added some more content :)

--- vpc/config.tf
+++ vpc/config.tf
@@ -17,9 +17,7 @@ provider "aws" {
 
 
 # Note-1
-
 # In general, profile is nothing but set of configuration settings(key–value pairs) which also includes 
 # username & password.
-
 # In the same way, AWS profile is a set of configuration settings(key–value pairs) which also includes 
 # username & password, that are used by the AWS CLI / AWS SDKs.
